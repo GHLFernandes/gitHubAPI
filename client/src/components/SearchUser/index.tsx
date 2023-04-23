4af72eb269135a430045b9092d3f4b3df8fd8a44
Gabriel Fernandes <fernandes.gh@outlook.com>
2023-04-23 19:02:59 -0300

    feat: searchForm

--- client/src/components/SearchUser/index.tsx
+++ client/src/components/SearchUser/index.tsx
@@ -6,37 +6,36 @@ interface Props {
     getUserData: (dataUser: IUser | undefined) => void
 }
 
-export const SearchUser: FunctionComponent<Props> = ({getUserData}) => {
+export const SearchUser: FunctionComponent<Props> = ({ getUserData }) => {
     const [user, setUser] = useState<string>('');
-    const [dataUser, setDataUser] = useState<IUser>();
+    const [dataUser, setDataUser] = useState<IUser | undefined>();
 
     useEffect(() => {
-        if(dataUser !== null && dataUser !== undefined){
+        if (dataUser !== undefined) {
             getUserData(dataUser);
         }
-    },[dataUser])
+    }, [dataUser])
 
-    const handleSubmit = () => {
-        try{
-            getUser(user).
-            then(data => setDataUser(data as IUser));
-        }catch(err){
-            console.error(err)
-        }finally{
-            setUser('')
-        }
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        getUser(user)
+            .then(data => setDataUser(data as IUser))
+            .catch(err => console.error(err))
+            .finally(() => setUser(''));
     }
 
-  return (
-    <>
-        <TextField 
-            id="searchGitUser" 
-            label="Search User" 
-            variant="standard" 
-            onChange={(e:React.ChangeEvent<HTMLInputElement>) => setUser(e.target.value)}
-            value={user}
-        />
-        <Button variant="outlined" onClick={handleSubmit}>Search</Button>
-    </>
-  )
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="searchGitUser"
+                    label="Search User"
+                    variant="standard"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUser(e.target.value)}
+                    value={user}
+                />
+                <Button variant="outlined">Search</Button>
+            </form>
+        </>
+    )
 }
